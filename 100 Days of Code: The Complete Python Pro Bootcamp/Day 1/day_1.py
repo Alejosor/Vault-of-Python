ae647fe014db99765083b40f9fab4e82876c2e02
alejosor <alejandrogabrielsorianopalo@gmail.com>
2024-11-27 22:46:32 -0500

    Avance #10 - Python Day 1

--- 100 Days of Code: The Complete Python Pro Bootcamp/Day 1/day_1.py
+++ 100 Days of Code: The Complete Python Pro Bootcamp/Day 1/day_1.py
@@ -2,6 +2,6 @@
 #Project - Day 1
 #Band Generator Project
 print("Welcome to the Band Name Generator")
-city=input("What is the name of the city that you grew up?\n")
+city=input("What is the name of the city that you grew up in?\n")
 pet=input("What is the name of your pet?\n")
 print(f"The name of your band could be,{city} {pet}")
