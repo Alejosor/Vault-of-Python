8efda635686614a9f1367c156a3c995595d06073
alejosor <alejandrogabrielsorianopalo@gmail.com>
2024-11-30 17:07:29 -0500

    Avance #16 - Python Day 1

---
+++ 100 Days of Code: The Complete Python Pro Bootcamp/Day 1/day_1.py
@@ -0,0 +1,23 @@
+#Printing
+print("Hello Wolrd")
+
+#String Manipulation
+print("This is a\nline")
+print("This is \ttabulation")
+
+#Inputs
+name = input("What's your name?")
+print(f"My name is {name}.") #Formating
+
+age_input = input("How old are you?")
+age = f"I'm {age_input} years old."
+
+#Rules for variables
+'''
+Make sure your variable names are descriptive
+Don't have spaces between words
+Don't start with numbers
+Don't use special words like print or input
+Choose simple words that are less likely to become typos
+Check the company style guidelines if you start work at a company
+'''
