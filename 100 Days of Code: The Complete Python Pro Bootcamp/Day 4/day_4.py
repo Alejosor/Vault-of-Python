10b173ef49147169556a58bfacb52867b9c2541c
alejosor <alejandrogabrielsorianopalo@gmail.com>
2024-12-01 22:39:24 -0500

    Avance #17 - Python Day 4

---
+++ 100 Days of Code: The Complete Python Pro Bootcamp/Day 4/day_4.py
@@ -0,0 +1,28 @@
+#Randomisation and Python Lists
+# #Random Module
+import random
+# import my_module
+
+# num1 = random.randint(1,10) #Random number between 1 and 10
+# print(num1)
+
+# #My Module
+# print(my_module.my_favorite_number) #Number -> 50
+
+# #Random number between 0 to 1
+# random_number_o_to_1 = random.random()
+# print(random_number_o_to_1)
+
+# #Random floating number
+# random_float = random.uniform(1,10)
+# print(random_float)
+
+#Heads of Tail
+# coin = random.randint(0,1)
+# print(f'The number was: {coin}')
+# if coin == 1:
+#     print("Heads")
+# else:
+#     print('Tails')
+
+#List
