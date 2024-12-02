dde0777b2b775ecf7da291d15725c23a0b3991e6
alejosor <alejandrogabrielsorianopalo@gmail.com>
2024-12-02 16:42:52 -0500

    Avance #18 - Python Day 4

--- 100 Days of Code: The Complete Python Pro Bootcamp/Day 4/day_4.py
+++ 100 Days of Code: The Complete Python Pro Bootcamp/Day 4/day_4.py
@@ -26,3 +26,39 @@ import random
 #     print('Tails')
 
 #List
+# fruits = ["orange","apple"]
+# states_of_america = ["Delaware", "Pennsylvania", "New Jersey", "Georgia", "Connecticut", "Massachusetts", "Maryland", "South Carolina", "New Hampshire", "Virginia", "New York", "North Carolina", "Rhode Island", "Vermont", "Kentucky", "Tennessee", "Ohio", "Louisiana", "Indiana", "Mississippi", "Illinois", "Alabama", "Maine", "Missouri", "Arkansas", "Michigan", "Florida", "Texas", "Iowa", "Wisconsin", "California", "Minnesota", "Oregon", "Kansas", "West Virginia", "Nevada", "Nebraska", "Colorado", "North Dakota", "South Dakota", "Montana", "Washington", "Idaho", "Wyoming", "Utah", "Oklahoma", "New Mexico", "Arizona", "Alaska", "Hawaii"]
+
+# print(states_of_america[13]) #Vermont
+# print(states_of_america[-1]) #Hawaii
+# print(states_of_america[-2]) #Alaska
+
+# #Change items in a list
+# states_of_america[1]="Penn State"
+# print(states_of_america[1])
+
+# #Add item at the end of the list
+# states_of_america.append('Alejosor')
+# print(states_of_america[-1])
+# print(states_of_america)
+
+# #Delete item of the list
+# states_of_america.pop(-1)
+# print(states_of_america)
+
+#Exercise - Who will pay the bill?
+#Option #1
+# friends = ['Alejo','Rosa','Lis','Luxo']
+# payer=random.randint(0,(len(friends)-1))
+# print('You are paying the bill this time',friends[payer])
+
+#Option #2
+# print('You are paying the bill this time',random.choice(friends))
+
+#Nested Lists
+fruits = ["Strawberries", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears"]
+vegetables = ["Spinach", "Kale", "Tomatoes", "Celery", "Potatoes"]
+ 
+dirty_dozen = [fruits, vegetables]
+ 
+print(dirty_dozen[1][1])
