cca02cb631dc3e7fa8849b520a54c90f3e251560
alejosor <alejandrogabrielsorianopalo@gmail.com>
2024-12-11 21:19:16 -0500

    Avance #34 - Python Day 6

---
+++ 100 Days of Code: The Complete Python Pro Bootcamp/Day 6/day_6.py
@@ -0,0 +1,24 @@
+## Functions ##
+
+#How to use
+"""
+def name_of_the_function:
+    * Do this *
+    * Then do this *
+    * Finally do this *
+"""
+# Calling Functions
+# my_function()
+
+# def get_user_name():
+#     name = input('Pls enter your name:\n')
+#     print(f'Welcome {name}, have a great day')
+
+# print('Welcome to the course')
+# get_user_name()
+
+# While Loop
+'''
+while something_is_true:
+    #Do something
+'''
