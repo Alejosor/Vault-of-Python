ec3af87529d9894a14d1d7da11ac2139c8b5fd50
alejosor <alejandrogabrielsorianopalo@gmail.com>
2025-03-30 08:27:04 -0500

    Primeros_Pasos

---
+++ Curso Intensivo de Python - Eric Matthes/Básico/1_Primeros_Pasos.py
@@ -0,0 +1,6 @@
+"""
+Primeros pasos para poder empezar con Python:
+    - Instalar Python -> https://www.python.org/
+    - Descargar un editor de texto o IDE (Entorno de desarrollo integrado) - Recomiendo -> https://code.visualstudio.com/
+    - Instalar las extensiones de Python en Visual Studio Code - Las siguientes -> Python, Python Debugger & Pylance
+"""
